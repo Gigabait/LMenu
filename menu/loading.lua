3347ce68b5217d1318ebf32735e1ead2efc009c4
Capster <m6277@yandex.ru>
2014-06-03 23:19:56 +0400

    Fixed Loading screen

--- menu/loading.lua
+++ menu/loading.lua
@@ -6,202 +6,155 @@ g_SteamID		= ""
 
 local PANEL = {}
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:Init()
 
-	self:SetSize( ScrW(), ScrH() )
+	self:SetSize(ScrW(), ScrH())
 
 end
 
 
-function PANEL:ShowURL( url, force )
-
-	if ( string.len( url ) < 5 ) then
-		return;
+function PANEL:ShowURL(url, force)
+	if #url < 5 then
+		return
 	end
 
-	if ( IsValid( self.HTML ) ) then 
-		if ( !force ) then return end
+	if IsValid(self.HTML) then 
+		if not force then return end
 		self.HTML:Remove() 
 	end
 
-	self:SetSize( ScrW(), ScrH() );
+	self:SetSize(ScrW(), ScrH())
 
-	self.HTML = vgui.Create( "DHTML", self )
-	self.HTML:SetSize( ScrW(), ScrH() );
-	self.HTML:Dock( FILL )
-	self.HTML:OpenURL( url )
+	self.HTML = vgui.Create("DHTML", self)
+	self.HTML:SetSize(ScrW(), ScrH())
+	self.HTML:Dock(FILL)
+	self.HTML:OpenURL(url)
 		
 	self:InvalidateLayout()
 	
 	self.LoadedURL = url
-
 end
 
 function PANEL:PerformLayout()
-
 	self:SetSize( ScrW(), ScrH() )
-	
 end
 
 
 function PANEL:Paint()
+	surface.SetDrawColor(30, 30, 30, 255)
+	surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
 
-	surface.SetDrawColor( 30, 30, 30, 255 )
-	surface.DrawRect( 0, 0, self:GetWide(), self:GetTall() )
-
-	if ( self.JavascriptRun && IsValid( self.HTML ) && !self.HTML:IsLoading() ) then
+	if self.JavascriptRun and IsValid( self.HTML ) and not self.HTML:IsLoading() then
 
 		-- MsgN( self.JavascriptRun )
 		self:RunJavascript( self.JavascriptRun )
 		self.JavascriptRun = nil
 
 	end
-	
 end
 
-
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:RunJavascript( str )
+	if not IsValid(self.HTML) then return end
+	if self.HTML:IsLoading() then return end
 
-	if ( !IsValid( self.HTML ) ) then return end
-	if ( self.HTML:IsLoading() ) then return end
-
-	self.HTML:RunJavascript( str );
-	
+	self.HTML:RunJavascript(str)
 end
 
-
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:OnActivate()
-
 	g_ServerName	= ""
 	g_MapName		= ""
 	g_ServerURL		= ""
 	g_MaxPlayers	= ""
 	g_SteamID		= ""
 
-	self:ShowURL( GetDefaultLoadingHTML() )
+	self:ShowURL(GetDefaultLoadingHTML())
 
 	self.NumDownloadables = 0
-
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:OnDeactivate()
 
-	if ( IsValid( self.HTML ) ) then self.HTML:Remove() end
+	if IsValid(self.HTML) then self.HTML:Remove() end
 	self.LoadedURL = nil
 	self.NumDownloadables = 0
 	
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
-function PANEL:Think()
-	
+function PANEL:Think()	
 	self:CheckForStatusChanges()
-	self:CheckDownloadTables()
-	
+	self:CheckDownloadTables()	
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
-function PANEL:StatusChanged( strStatus )
-
-	if ( string.find( strStatus, "Downloading " ) ) then
+function PANEL:StatusChanged(strStatus)
+	if string.find( strStatus, "Downloading " ) then
+		local Filename = string.gsub(strStatus, "Downloading ", "")
+		Filename = string.gsub(Filename, "'", "\\'")
+		self:RunJavascript("if ( window.DownloadingFile ) DownloadingFile( '" .. Filename .. "' )")
+		return 
+	end
 	
-		local Filename = string.gsub( strStatus, "Downloading ", "" )
-		Filename = string.gsub( Filename, "'", "\\'" )
-		self:RunJavascript( "if ( window.DownloadingFile ) DownloadingFile( '" .. Filename .. "' )" );
-	
-	return end
-	
-	strStatus = string.gsub( strStatus, "'", "\\'" )
-	self:RunJavascript( "if ( window.SetStatusChanged ) SetStatusChanged( '" .. strStatus .. "' )" );
-	
+	strStatus = string.gsub(strStatus, "'", "\\'")
+	self:RunJavascript("if ( window.SetStatusChanged ) SetStatusChanged( '" .. strStatus .. "' )")
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:CheckForStatusChanges()
-
 	local str = GetLoadStatus()
-	if ( !str ) then return end
+	if not str then return end
 	
-	str = string.Trim( str )
-	str = string.Trim( str, "\n" )
-	str = string.Trim( str, "\t" )
+	str = string.Trim(str)
+	str = string.Trim(str, "\n" )
+	str = string.Trim(str, "\t" )
 	
-	str = string.gsub( str, ".bz2", "" )
-	str = string.gsub( str, ".ztmp", "" )
-	str = string.gsub( str, "\\", "/" )
+	str = string.gsub(str, ".bz2", "")
+	str = string.gsub(str, ".ztmp", "")
+	str = string.gsub(str, "\\", "/")
 	
-	if ( self.OldStatus && self.OldStatus == str ) then return end
+	if self.OldStatus and self.OldStatus == str then return end
 	
 	self.OldStatus = str
-	self:StatusChanged( str )
-
+	self:StatusChanged(str)
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:RefreshDownloadables()
 
 	self.Downloadables = GetDownloadables()
-	if ( !self.Downloadables ) then return end
+	if not self.Downloadables then return end
 	
 	local iDownloading = 0
 	local iFileCount = 0
-	for k, v in pairs( self.Downloadables ) do
 	
-		v = string.gsub( v, ".bz2", "" )
-		v = string.gsub( v, ".ztmp", "" )
-		v = string.gsub( v, "\\", "/" )
+	for k, v in pairs(self.Downloadables) do
+		v = string.gsub(v, ".bz2", "")
+		v = string.gsub(v, ".ztmp", "")
+		v = string.gsub(v, "\\", "/")
 	
-		iDownloading = iDownloading + self:FileNeedsDownload( v )
+		iDownloading = iDownloading + self:FileNeedsDownload(v)
 		iFileCount = iFileCount + 1
-
 	end
 	
-	if ( iDownloading == 0 ) then return end
+	if iDownloading == 0 then return end
 	
-	self:RunJavascript( "if ( window.SetFilesNeeded ) SetFilesNeeded( " .. iDownloading .. ")" );
-	self:RunJavascript( "if ( window.SetFilesTotal ) SetFilesTotal( " .. iFileCount .. ")" );
+	self:RunJavascript( "if ( window.SetFilesNeeded ) SetFilesNeeded( "..iDownloading..")" )
+	self:RunJavascript( "if ( window.SetFilesTotal ) SetFilesTotal( "..iFileCount..")" )
 
 end
 
 function PANEL:FileNeedsDownload( filename )
 
 	local iReturn = 0
-	local bExists = file.Exists( filename, "GAME" )
-	if ( bExists ) then	return 0 end
+	local bExists = file.Exists(filename, "GAME")
+	if bExists then	return 0 end
 	
 	return 1
 	
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:CheckDownloadTables()
 
 	local NumDownloadables = NumDownloadables()
-	if ( !NumDownloadables ) then return end
+	if not NumDownloadables then return end
 	
-	if ( self.NumDownloadables && NumDownloadables == self.NumDownloadables ) then return end
+	if self.NumDownloadables and NumDownloadables == self.NumDownloadables then return end
 		
 	self.NumDownloadables = NumDownloadables
 	self:RefreshDownloadables()
@@ -214,24 +167,21 @@ local pnlLoading = nil
 
 function GetLoadPanel()
 
-	if ( !IsValid( pnlLoading ) ) then
-		pnlLoading = vgui.CreateFromTable( PanelType_Loading )
+	if not IsValid(pnlLoading) then
+		pnlLoading = vgui.CreateFromTable(PanelType_Loading)
 	end
 
 	return pnlLoading
 	
 end
 
-function UpdateLoadPanel( strJavascript )
-	
-	if ( !pnlLoading ) then return end
-
-	pnlLoading:RunJavascript( strJavascript )
-
+function UpdateLoadPanel(strJavascript)	
+	if not pnlLoading then return end
+	pnlLoading:RunJavascript(strJavascript)
 end
 
 
-function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode )
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode)
 
 	if engine.IsPlayingDemo() then return end
 
@@ -242,21 +192,21 @@ function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamem
 	g_SteamID		= steamid
 	g_GameMode		= gamemode
 
-	MsgN( servername )
-	MsgN( serverurl )
-	MsgN( gamemode )
-	MsgN( mapname )
-	MsgN( maxplayers )
-	MsgN( steamid )
+	MsgC(Color(0xAA, 0xAA, 0xFF), servername.."\n")
+	MsgC(Color(0xAA, 0xAA, 0xFF), serverurl.."\n")
+	MsgC(Color(0xAA, 0xAA, 0xFF), gamemode.."\n")
+	MsgC(Color(0xAA, 0xAA, 0xFF), mapname.."\n")
+	MsgC(Color(0xAA, 0xAA, 0xFF), maxplayers.."\n")
+	MsgC(Color(0xAA, 0xAA, 0xFF), steamid.."\n")
 	
-	serverurl = serverurl:Replace( "%s", steamid )
-	serverurl = serverurl:Replace( "%m", mapname )
+	serverurl = serverurl:Replace("%s", steamid)
+	serverurl = serverurl:Replace("%m", mapname)
 
-	if ( maxplayers > 1 ) then 
+	if maxplayers > 1 then 
 		pnlLoading:ShowURL( serverurl, true ) 
 	end
 
-	pnlLoading.JavascriptRun = string.format( "if ( window.GameDetails ) GameDetails( \"%s\", \"%s\", \"%s\", %i, \"%s\", \"%s\" );",
-		servername:JavascriptSafe(), serverurl:JavascriptSafe(), mapname:JavascriptSafe(), maxplayers, steamid:JavascriptSafe(), g_GameMode:JavascriptSafe() )
+	pnlLoading.JavascriptRun = string.format("if ( window.GameDetails ) GameDetails( \"%s\", \"%s\", \"%s\", %i, \"%s\", \"%s\" );",
+		servername:JavascriptSafe(), serverurl:JavascriptSafe(), mapname:JavascriptSafe(), maxplayers, steamid:JavascriptSafe(), g_GameMode:JavascriptSafe())
 
 end
